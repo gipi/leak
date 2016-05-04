0b114c1fa515cd2d8ff882107ab426015d0b4dea
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2016-05-04 22:58:45 +0200

    Use unsigned formatter.

--- leak/utils.py
+++ leak/utils.py
@@ -2,4 +2,4 @@ import struct
 
 # I don't know because use "i" fmt (see <https://docs.python.org/2.7/library/struct.html>)
 def convert_address_to_string(address):
-    return struct.pack("i", address)
+    return struct.pack("I", address)
