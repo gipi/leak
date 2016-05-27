1f7083953e3645564a8c102f6b30f91dc43170aa
Gianluca Pacchiella <pacchiella@realisti.co>
2016-05-27 17:15:27 +0200

    Rework core classes.

--- leak/base.py
+++ leak/base.py
@@ -10,20 +10,23 @@ class Parser(object):
 
 
 class RegexParser(Parser):
-    def __init__(self, pattern):
+    '''Parser class that simply returns the data of interest from a leaking
+    service'''
+    def __init__(self, pattern, regex_flags=None):
         self.pattern = pattern
+        self.regex_flags = regex_flags or re.DOTALL|re.M
+
+        self.regex = re.compile(self.pattern, self.regex_flags)
 
     def get_leak(self, data):
-        pattern = re.compile(self.pattern, re.DOTALL|re.M)
-
-        leak = ""
+        leak = None
         try:
-            leak = pattern.findall(data)[0]
+            leak = self.regex.match(data)
         except Exception as e:
             print >> sys.stderr, " [!]", data
             raise e
 
-        return leak
+        return leak.groupdict()
 
 
 
@@ -31,48 +34,81 @@ class LeakerEOS(Exception):
     '''Indicates that the stream doesn't have more data of interest'''
     pass
 
-class Leaker(object):
+class BaseLeaker(object):
+    '''
+    This is the base class to use in order to create you own leaker.
+
+    Roughly speaking the leaker launch via some channel sequentially
+    some requests and extract the leaking data in order to reconstruct
+    a certain global state of the underlying system to be exploited.
+
+    Each request is called a **session**.
+
+    The request content is manipulated using a Parser that extracts
+    the essential data.
+
+    TODO: how to make all of this multi-threading?
+    '''
     def __init__(self, parser=None, **kwargs):
         if not parser:
             raise AttributeError('you must pass a valid parser instance')
 
         self.parser = parser
 
-    def input(self):
+        # this two below are for session data
+        self._data = None # this will contain the original data before parsing
+        self._leak = None # this will contain the data extracted
+
+        # this is a global state
+        self._state = None # this will contain the data structure to be reconstructed from the leaks
+
+    def input(self, args, **kwargs):
         '''Here is where you call your channel and return some data to elaborate upon'''
         raise NotImplementedError('you have to implement input()')
 
-    def get_next_input_parameter(self):
+    def get_next_input_parameters(self):
         raise NotImplementedError('you have to implemement get_next_input_parameter()')
 
-    def update(self, leak):
-        """Update internal state after a leak"""
-        return leak
+    def update(self):
+        """Update internal state after a leak using the dictionary returned from the parser."""
+        raise NotImplementedError('you have to implement update()')
 
-    def output(self, leak_piece):
-        """Decides howto emit the leak"""
-        raise NotImplementedError('you must implement output()')
+    @property
+    def state(self):
+        '''
+        Access the internal reconstructed data
+        '''
+        return self._state
 
     def extract(self):
-        return self.parser.get_leak(self.input())
+        self._data = self.input(self.get_next_input_parameters())
+        self._leak = self.parser.get_leak(self._data)
 
     def has_finished(self):
-        return False
+        return NotImplementedError('you have to implement has_finished()')
 
-    def on_exit(self):
+    def on_session_start(self):
+        pass
+
+    def on_session_end(self):
+        pass
+
+    def on_end(self):
         pass
 
     def __call__(self):
         while not self.has_finished():
+            # TODO: factorize inner code as an iterator
             try:
-                representation = self.extract() # here we obtain the first representation from the parser
-                representation = self.update(representation) # here we elaborate the text to create some representation
-                self.output(representation)
+                self.on_session_start()
+
+                self.extract() # here we obtain the first representation from the parser
+                self.update() # here we elaborate the text to create some representation
+
+                self.on_session_end()
             except LeakerEOS as e:
                 break
             except KeyboardInterrupt:
                 break
-            #    print >> sys.stderr, "fatal:", e
-            #    raise e
 
-        self.on_exit()
+        self.on_end()
